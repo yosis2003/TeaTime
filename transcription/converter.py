05307cd2ba861c69d8ab642021ae84a166f78905
yosis <78387492+yosis2003@users.noreply.github.com>
2025-11-15 20:53:01 -0800

    made converter script

---
+++ transcription/converter.py
@@ -0,0 +1,21 @@
+import os
+import ffmpeg
+
+
+
+
+def converter(dirpath, newDirectory, name):
+    newName = name[:-4] + ".mp3"
+    ffmpeg.input(dirpath + "/" + name).output(newDirectory + "/" + newName).run()
+
+
+def traverser():
+    startingDirectory = "/home/yosis/Videos/trainingvids"
+    newOne = "/home/yosis/Music/converted"
+    for root, directories, filenames in os.walk(startingDirectory):
+        for name in filenames:
+            if name.endswith(".mp4"):
+                converter(root, newOne, name)
+
+if __name__ == "__main__":
+    traverser()
